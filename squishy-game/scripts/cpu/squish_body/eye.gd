820f069ddc8aa0a27d1f8bd3bda72fa2a04d62c6
Baptiste Girardin <baptiste.girardin@telecom-paris.fr>
2026-05-24 19:58:51 +0200

    Add jump buffer

--- squishy-game/scripts/cpu/squish_body/eye.gd
+++ squishy-game/scripts/cpu/squish_body/eye.gd
@@ -9,12 +9,10 @@ var center: Vector3
 func _process(delta: float) -> void:
 	var offset: Vector3 = (global_position - center).normalized()
 	center = squishy.global_transform * squishy.pos_center
-	var new_offset: Vector3 = offset.slerp(last_dir, 20 * delta)
+	var new_offset: Vector3 = offset.slerp(last_dir, 8 * delta)
 	var radius: float = squishy.get_radius_in_dir(squishy.global_transform.basis.transposed() * new_offset)
 	global_position = center + new_offset * radius
 	
-	var move_dir: Vector3 = squishy.glob_vel
-	move_dir.y = 0
-	move_dir = move_dir.normalized()
-	if move_dir.length_squared() < 1e-8: return
-	last_dir = move_dir.normalized()
+	var move_force: Vector3 = move_squishy.get_move_force()
+	if move_force.length_squared() < 1e-8: return
+	last_dir = move_force.normalized()
--- squishy-game/scripts/cpu/squish_body/jump_squishy.gd
+++ squishy-game/scripts/cpu/squish_body/jump_squishy.gd
@@ -3,13 +3,20 @@ extends Node
 @export var squishy: Squishy
 @export var jump_force: float
 @export var coyotee_time: float
+@export var jump_buffer_time: float
 
+# CAN JUMP
 const jump_cd: float = 0.2
 var can_jump: bool = true
 var jump_timer: float
 
+# COYOTEE
 var can_jump_coyotee: bool
 var coyotee_timer: float
+
+# BUFFER
+var buffer_counter: float
+var is_jumping: bool 
 
 func _process(dt: float) -> void:
 	if jump_timer > jump_cd:
@@ -24,9 +31,19 @@ func _process(dt: float) -> void:
 			can_jump_coyotee = false
 		else: coyotee_timer += dt
 		
-	if !InputManager.jumps() or !can_jump_coyotee or !can_jump:
+	if buffer_counter > jump_buffer_time:
+		is_jumping = false
+	else:
+		buffer_counter += dt
+	if is_jumping: print(is_jumping)
+		
+	if !is_jumping or !can_jump_coyotee or !can_jump:
 		return
 	can_jump = false
 	squishy.add_glob_acc(Vector3.UP * jump_force / dt)
 	squishy.add_glob_vel(-Vector3.DOWN * Vector3.DOWN.dot(squishy.glob_vel))
 	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		is_jumping = true
+		buffer_counter = 0
