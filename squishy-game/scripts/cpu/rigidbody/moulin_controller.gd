d5661f21e4e8321ea015c768bedf572f3504d980
Elyes Jemel <elyes.jemel@telecom-paris.fr>
2026-06-25 12:45:23 +0200

    Add windmill

---
+++ squishy-game/scripts/cpu/rigidbody/moulin_controller.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var parent : Rigidbody
+@export var rotation_speed : int = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var local_VecZ = (parent.global_transform.basis * Vector3.FORWARD).normalized()
+	
+	parent.add_angular_vel(rotation_speed * local_VecZ)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.s
+func _process(delta: float) -> void:
+	
+	pass
