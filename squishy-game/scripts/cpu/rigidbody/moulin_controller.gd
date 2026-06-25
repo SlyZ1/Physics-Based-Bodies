1f216c2aab1eb34a9db434371c93ce723a6f2253
Baptiste Girardin <baptiste.girardin@telecom-paris.fr>
2026-06-25 13:10:43 +0200

    Fix moulin parent node

--- squishy-game/scripts/cpu/rigidbody/moulin_controller.gd
+++ squishy-game/scripts/cpu/rigidbody/moulin_controller.gd
@@ -1,12 +1,12 @@
 extends Node
 
-@export var parent : Rigidbody
+@export var child : Rigidbody
 @export var rotation_speed : float = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var local_VecZ = (parent.global_transform.basis * Vector3.FORWARD).normalized()
+	var local_VecZ = (child.global_transform.basis * Vector3.FORWARD).normalized()
 	
-	parent.add_angular_vel(rotation_speed * local_VecZ)
+	child.add_angular_vel(rotation_speed * local_VecZ)
 	pass # Replace with function body.
 
 
