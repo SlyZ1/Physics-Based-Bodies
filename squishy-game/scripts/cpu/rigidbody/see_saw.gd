c5e89f4f1143c2036126fe79ebf758248d114edb
Elyes Jemel <elyes.jemel@telecom-paris.fr>
2026-06-25 17:19:27 +0200

    fix see-saw rotation bug

--- squishy-game/scripts/cpu/rigidbody/see_saw.gd
+++ squishy-game/scripts/cpu/rigidbody/see_saw.gd
@@ -1,18 +1,20 @@
 extends Node3D
 
-@export var child : Rigidbody
-@export var rotation_threshold : float = PI / 6
+@onready var child := $Board
+@export var mass : float
+@export var rotation_threshold : float = PI / 6	
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	child.m = mass
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var local_VecZ = child.global_transform.basis.z.normalized()
+	var global_VecZ = child.global_basis.z
 	var angle = child.rotation[2] - PI / 2.0
 	var sgn = 1
 	if angle < 0:
@@ -20,6 +22,6 @@ func _process(delta: float) -> void:
 		
 	if abs(angle) > rotation_threshold:
 		child.rotate_z(-1 * sgn * (abs(angle) - rotation_threshold))
-		if child.glob_angular_vel.z * sgn > 0:
-			child.glob_angular_vel.z = 0
+		if -1 * sgn * child.glob_angular_vel.dot(global_VecZ) < 0:
+			child.glob_angular_vel = Vector3.ZERO
 	pass
